4dca5856f62ebe93cb19e7af2e4e306c5a04ba18
AlejandroAgueroDev <alejandroaguerodev@gmail.com>
2025-07-29 20:47:11 -0300

    en local

--- src/common/URL_SERVER.js
+++ src/common/URL_SERVER.js
@@ -1,2 +1,2 @@
-export const url = "https://sistema-tambo-backend.onrender.com/";
-// export const url = "http://localhost:3000/";
+// export const url = "https://sistema-tambo-backend.onrender.com/";
+export const url = "http://localhost:3000/";
